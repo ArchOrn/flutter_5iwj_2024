e35c468043f67590be33bea1b35ab3746f6eb49a
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-09-19 12:12:44 +0200

    Demo with StatefulWidget and another example with separate square

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_5iwj_2024/home/square.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Color _color = Colors.red;
+
+  void _changeColor() {
+    setState(() => _color = _color == Colors.red ? _color = Colors.amber : Colors.red);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: const Center(
-        child: Square(),
+      body: Center(
+        child: Square(color: _color),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
         shape: const CircleBorder(),
-        onPressed: () {},
+        onPressed: _changeColor,
         child: const Icon(
           Icons.swap_calls,
           color: Colors.white,
--- lib/home/square.dart
+++ lib/home/square.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Square extends StatelessWidget {
-  const Square({super.key});
+  final Color color;
+
+  const Square({super.key, this.color = Colors.red});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 50,
       height: 50,
-      color: Colors.red,
+      color: color,
     );
   }
 }
