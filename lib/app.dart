2f6a4101418cf98edac66b7491a45ab33ced7a0b
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-09-19 08:48:03 +0200

    Demo with custom InheritedWidget

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Home(),
+      home: const AppData(
+        child: Home(),
+      ),
       theme: ThemeData(
         textTheme: const TextTheme(
           displayMedium: TextStyle(
@@ -26,11 +28,29 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Hello',
-        style: Theme.of(context).textTheme.displayMedium,
+    return Container(
+      color: AppData.of(context).myColor,
+      child: Center(
+        child: Text(
+          'Hello',
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
       ),
     );
+  }
+}
+
+class AppData extends InheritedWidget {
+  static AppData of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<AppData>()!;
+  }
+
+  final Color myColor = const Color(0xffd50f34);
+
+  const AppData({super.key, required super.child});
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
   }
 }
