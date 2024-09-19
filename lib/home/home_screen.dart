5bfd76be76304532330b077d9bd4a65bedb36a3f
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-09-19 12:36:08 +0200

    Base for Layouts

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_5iwj_2024/home/square.dart';
+import 'package:flutter_5iwj_2024/home/widgets/red_square.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Color _color = Colors.white;
-
-  void _changeColor() {
-    setState(() => _color = _color == Colors.white ? _color = Colors.black : Colors.white);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: _color,
+    return const Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        child: Square(
-          onTap: _changeColor,
-        ),
+        child: RedSquare(),
       ),
     );
   }
