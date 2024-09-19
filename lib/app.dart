2b5dd966b8b643b4fef06a5b237d49330b086805
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-09-19 10:33:09 +0200

    Demo with Scaffold and base components

--- lib/app.dart
+++ lib/app.dart
@@ -7,10 +7,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AppData(
-        child: Home(),
-      ),
+      home: const Home(),
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         textTheme: const TextTheme(
           displayMedium: TextStyle(
             color: Colors.amber,
@@ -18,6 +17,7 @@ class App extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        useMaterial3: true,
       ),
     );
   }
@@ -28,29 +28,53 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppData.of(context).myColor,
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.flight,
+            color: Colors.white,
+          ),
+        ),
+        title: const Text(
+          'My Title',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
         child: Text(
           'Hello',
           style: Theme.of(context).textTheme.displayMedium,
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        shape: const CircleBorder(),
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
     );
-  }
-}
-
-class AppData extends InheritedWidget {
-  static AppData of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<AppData>()!;
-  }
-
-  final Color myColor = const Color(0xffd50f34);
-
-  const AppData({super.key, required super.child});
-
-  @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    return true;
   }
 }
