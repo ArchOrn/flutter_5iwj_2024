729b27e2346dedc56102cfc3a9faaf85b4d7f3bc
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-10-17 14:24:16 +0200

    Demo with web services and FutureBuilder

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,84 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_5iwj_2024/core/exceptions/app_exception.dart';
-import 'package:flutter_5iwj_2024/core/models/post.dart';
 import 'package:flutter_5iwj_2024/core/services/api_services.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<Post> _posts = [];
-  bool _loading = false;
-  AppException? _error;
-
-  @override
-  void initState() {
-    super.initState();
-
-    setState(() => _loading = true);
-    ApiServices.getPosts().then((data) {
-      _posts = data;
-    }).catchError((error) {
-      _error = AppException.from(error);
-    }).whenComplete(() {
-      setState(() => _loading = false);
-    });
-
-    // WidgetsBinding.instance.addPostFrameCallback((timestamp) async {
-    //   setState(() => _loading = true);
-    //   try {
-    //     _posts = await ApiServices.getPosts();
-    //   } catch (error) {
-    //     _error = AppException.from(error);
-    //   }
-    //   setState(() => _loading = false);
-    // });
-  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Builder(builder: (context) {
-          if (_loading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          // Note: better nullable handling
-          // final error = _error;
-          // if (error != null) {
-          //   return Center(
-          //     child: Text(error.message),
-          //   );
-          // }
-
-          if (_error != null) {
-            return Center(
-              child: Text(_error!.message),
-            );
-          }
-
-          return ListView.separated(
-            itemBuilder: (context, index) {
-              final post = _posts[index];
-              return ListTile(
-                title: Text(post.title),
-                subtitle: Text(post.body),
+        body: FutureBuilder(
+          future: ApiServices.getPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(height: 8);
-            },
-            itemCount: _posts.length,
-          );
-        }),
+            }
+
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  AppException.from(snapshot.error).message,
+                ),
+              );
+            }
+
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                final post = snapshot.data![index];
+                return ListTile(
+                  title: Text(post.title),
+                  subtitle: Text(post.body),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 8);
+              },
+              itemCount: snapshot.data!.length,
+            );
+          },
+        ),
       ),
     );
   }
