b3b8a000ac27e5f69a9e23a11e6703d1e7cc91e7
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-10-17 10:34:46 +0200

    Demo with Layouts

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_5iwj_2024/home/widgets/red_circle.dart';
 import 'package:flutter_5iwj_2024/home/widgets/red_square.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,25 +7,73 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(
-          color: Colors.amber,
-          child: const Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: RedSquare(),
-              ),
-              SizedBox(width: 20),
-              Expanded(
-                flex: 1,
-                child: RedSquare(),
-              ),
-            ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.orange,
+                    padding: const EdgeInsets.all(12),
+                    child: Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: [
+                        ...List.generate(22, (index) {
+                          return const RedCircle();
+                        }),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: double.infinity,
+                        color: Colors.amber,
+                        padding: const EdgeInsets.all(12),
+                        child: const Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RedCircle(),
+                            SizedBox(width: 8),
+                            RedCircle(),
+                            Spacer(),
+                            RedCircle(),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+                const RedSquare(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
