74657cc70ae47bd7edc94f6b7fb8624c6eb3050f
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-11-14 11:54:34 +0100

    Reset

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_5iwj_2024/core/exceptions/app_exception.dart';
-import 'package:flutter_5iwj_2024/core/services/api_services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,58 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        body: FutureBuilder(
-          future: ApiServices.getRecipes(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  AppException.from(snapshot.error).message,
-                ),
-              );
-            }
-
-            return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemBuilder: (context, index) {
-                final recipe = snapshot.data![index];
-                return Stack(
-                  children: [
-                    Image.network(recipe.imageUrl),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        color: Colors.black.withOpacity(.5),
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: Text(
-                          recipe.name,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                );
-              },
-              itemCount: snapshot.data!.length,
-            );
-          },
-        ),
+        body: Container(),
       ),
     );
   }
