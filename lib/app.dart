3eedb19e3a32216a47b7aa5ea225c9e03a96b51a
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-11-15 08:23:59 +0100

    Reset

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_5iwj_2024/article/article_screen.dart';
 import 'package:flutter_5iwj_2024/home/home_screen.dart';
 
 class App extends StatelessWidget {
@@ -11,36 +10,6 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => const HomeScreen(),
-      },
-      onGenerateRoute: (routeSettings) {
-        switch (routeSettings.name) {
-          case ArticleScreen.routeName:
-            return PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) {
-                return ArticleScreen(id: routeSettings.arguments as String);
-              },
-              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                const begin = Offset(0.0, 1.0);
-                const end = Offset.zero;
-                final tween = Tween(begin: begin, end: end);
-                final opacityTween = CurveTween(curve: Curves.easeInOut);
-
-                animation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
-                final offsetAnimation = animation.drive(tween);
-                final opacityAnimation = animation.drive(opacityTween);
-
-                return SlideTransition(
-                  position: offsetAnimation,
-                  child: FadeTransition(
-                    opacity: opacityAnimation,
-                    child: child,
-                  ),
-                );
-              },
-            );
-        }
-
-        return null;
       },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,35 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_5iwj_2024/article/article_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        color: Colors.amber,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Home screen',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => ArticleScreen.navigateTo(context, id: 'toto-tata'),
-              child: const Text(
-                'Go to article',
-              ),
-            ),
-          ],
-        ),
-      ),
+    return const Scaffold(
+      backgroundColor: Colors.white,
     );
   }
 }
